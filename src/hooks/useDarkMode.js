8a72af333aa59698da62c8973be4621e9c537044
spenclark <spenclark@hotmail.com>
2019-10-31 12:09:17 -0700

    intial

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,22 @@
+import  { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue)
+
+    const handleChanges = updatedValue => {
+        setDarkMode(updatedValue)
+    };
+
+
+
+    useEffect(() => {
+        if(darkMode ===true) {
+            return document.querySelector('body').classList.add('dark-mode')
+        } else {
+            return document.querySelector('body').classList.remove('dark-mode')
+        }
+    },[darkMode])
+
+    return [darkMode, setDarkMode, handleChanges];
+}
